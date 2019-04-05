ca0d3ddaee2cf2dfd5015c263ed44bcb7152f7cd
Sorin Chis <kish.sorin@yahoo.com>
2019-04-05 10:58:59 +0300

    added Login Component and axios function

--- react-client-app/src/App.js
+++ react-client-app/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Register from './components/Register';
+import Login from './components/Login';
+import JokesList from './components/JokesList';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Dad Jokes - Sprint Challenge</h1>
+{/* 
+				<Route
+					exact
+					path="/jokes"
+					render={(props) =>
+						localStorage.getItem('token') ? <JokesList {...props} /> : <Redirect to="/login" />}
+				/> */}
 
 				<div>
 					<Route path="/register" component={Register} />
-					{/* <Route path="/login" component={Login} /> */}
+					<Route path="/login" component={Login} />
 				</div>
 			</div>
 		);
